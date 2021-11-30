979830f4b0f9800c08ae461500873afca1e3e678
William Gao <gaoyi.william@gmail.com>
2021-12-01 02:05:26 +0800

    all you

--- A4/out/src/e2fs.c
+++ A4/out/src/e2fs.c
@@ -75,16 +75,51 @@ char** get_path_and_name(char* trimmed_path){
 unsigned find_last_inode(char *dir_path){
     // Find inode of the last dir in dir_path
     // exits with proper error if one of them does not exist or is a file
-    char* current_path = calloc(strlen(dir_path) + 1, sizeof(char));
-    char* current_dir;
-    
-    
-    return 0;
+    int length = strlen(dir_path);
+    char* current_path = calloc(length + 1, sizeof(char));
+    strncpy(current_path, dir_path, length);
+    current_path += 1;
+    char* current_name = calloc(length + 1, sizeof(char));
+    get_curr_dir_name(&current_path, &current_name);
+    unsigned int inode_index = EXT2_ROOT_INO - 1;
+    struct ext2_inode* inode_entry = get_inode_by_index(inode_index);
+
+    while(strlen(current_path) != 0){
+        if(inode_entry->i_mode & EXT2_S_IFDIR){
+            struct ext2_dir_entry* dir_entry = get_dir_entry();
+            inode_index = dir_entry->inode - 1;
+			inode_entry = get_inode_by_index(inode_index);
+        }
+        else{
+            exit(ENOENT);
+        }
+    }
+    return inode_index;
 }
 
-char* get_curr_dir_name(char** current_path){
+void get_curr_dir_name(char** current_path, char** current_name){
     // modify current_path, and return curr_dir_name
     // e.g. current_path = "/foo/bar/lol/"
     // returns "foo", and modifies current_path to /bar/lol/
+    if (strlen(*current_path) == 0){
+       *current_name = NULL;
+       return;
+    }
+    char* temp = calloc(strlen(*current_path), sizeof(char));
+    strcpy(temp, *current_path);
+    char* token = strtok(temp, "/");
+    strcpy(*current_name, token);
+    *current_path += strlen(token) + 1;
+    free(temp);
+}
+
+struct ext2_inode* get_inode_by_index(unsigned int index){
+    struct ext2_inode *inode_table = (struct ext2_inode *) (disk + EXT2_BLOCK_SIZE * gd->bg_inode_table);
+    return (struct ext2_inode*)(&inode_table[index]);
+}
+
+struct ext2_dir_entry* get_dir_entry(){
+    // TODO: implement this with proper variables
+    // exits with proper errors, e.g. exit(ENOENT)
     
 }
