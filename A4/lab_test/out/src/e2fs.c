caa1c1d79789a247d15852db41ab35c2af908bd3
Connor315 <gao315320130@gmail.com>
2021-12-04 08:59:30 -0500

    bug

--- A4/lab_test/out/src/e2fs.c
+++ A4/lab_test/out/src/e2fs.c
@@ -285,7 +285,7 @@ void init_new_dir_in_old_block(struct ext2_dir_entry * dir_entry, char* dir_name
 
     //Initialize .
     //might have problem here.
-    struct ext2_dir_entry * new_dir_entry = (struct ext2_dir_entry *) (disk + 1024 * unused_block_num);
+    struct ext2_dir_entry * new_dir_entry = (struct ext2_dir_entry *) (disk + 1024 * (unused_block_num + 1));
     init_first_dir(new_dir_entry, dir_entry->inode);
 
     //Initialize ..
@@ -293,7 +293,7 @@ void init_new_dir_in_old_block(struct ext2_dir_entry * dir_entry, char* dir_name
     init_second_dir(dir_entry, parent_inode);
 
     struct ext2_inode ext2_inode = inode_table[dir_entry->inode - 1];
-    update_inode_blocks(&ext2_inode, unused_block_num);
+    update_inode_blocks(&ext2_inode, unused_block_num+1);
 }
 
 void update_inode_blocks(struct ext2_inode *inode, int unused_block_num){
