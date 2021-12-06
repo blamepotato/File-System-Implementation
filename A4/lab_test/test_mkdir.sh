ec5d64b6ad7980b77f8c594bca3d35a15c570c3e
William Gao <gaoyi.william@gmail.com>
2021-12-06 12:50:26 +0800

    fe

--- A4/lab_test/test_mkdir.sh
+++ A4/lab_test/test_mkdir.sh
@@ -2,42 +2,43 @@
 cd out/
 # 17 level 2 exists
 cd util/
-./ext2umfs_mkdir /level1/level2/
+./ext2umfs_mkdir /level1/level2/; echo "here:"$?
 cd ..
 cd img/
 ./ext2_dump twolevel.img
 # 2 not abs path 
 cd ..
 cd util/
-./ext2umfs_mkdir level1/level2/level3
+./ext2umfs_mkdir level1/level2/level3; echo "here:"$?
 cd ..
 cd img/
 ./ext2_dump twolevel.img
 # 2 is a file
 cd ..
 cd util/
-./ext2umfs_mkdir /level1/level2/bfile
+./ext2umfs_mkdir /level1/level2/bfile; echo "here:"$?
 cd ..
 cd img/
 ./ext2_dump twolevel.img
+echo $?
 # 2 path doesn't exist
 cd ..
 cd util/
-./ext2umfs_mkdir /level1/level100/level5
+./ext2umfs_mkdir /level1/level100/level5; echo "here:"$?
 cd ..
 cd img/
 ./ext2_dump twolevel.img
 # no error 
 cd ..
 cd util/
-./ext2umfs_mkdir //level1//level2////level3/////
+./ext2umfs_mkdir //level1//level2////level3/////; echo "here:"$?
 cd ..
 cd img/
 ./ext2_dump twolevel.img
 # 17 exists 
 cd ..
 cd util/
-./ext2umfs_mkdir //level1//level2////level3/////
+./ext2umfs_mkdir //level1//level2////level3/////; echo "here:"$?
 cd ..
 cd img/
 ./ext2_dump twolevel.img
