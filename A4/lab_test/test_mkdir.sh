593be4733ae535cb2545da4bb18c2b423bd4b8d7
William Gao <gaoyi.william@gmail.com>
2021-12-06 11:36:13 +0800

    last test

--- A4/lab_test/test_mkdir.sh
+++ A4/lab_test/test_mkdir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd out/
 cd util/
-./ext2umfs_mkdir /level1/level2/
+./ext2umfs_mkdir ////level1////level2/////level99////
 cd ..
 cd img/
-./ext2_dump onedirectory.img
+./ext2_dump twolevel.img
