de31a200e878b480c467bc75468962a88268c31e
William Gao <gaoyi.william@gmail.com>
2021-12-09 08:49:49 +0800

    ln

--- A4/lab_test/out/src/ext2fsal_ln_hl.c
+++ A4/lab_test/out/src/ext2fsal_ln_hl.c
@@ -30,8 +30,73 @@ int32_t ext2_fsal_ln_hl(const char *src,
      */
 
      /* This is just to avoid compilation warnings, remove these 3 lines when you're done. */
-    (void)src;
-    (void)dst;
+    // 1. check and reformat input path
+    int error = 0;
+    int src_has_slash = 0;
+    int dst_has_slash = 0;
+    char src_copy[strlen(src) + 1];
+    strcpy(src_copy, src);
+    src_copy[strlen(src_copy)] = '\0';
+
+    char dst_copy[strlen(dst) + 1];
+    strcpy(dst_copy, dst);
+    dst_copy[strlen(dst_copy)] = '\0';
+
+    char* src_trimmed_path = escape_path(dst_copy, &error, &src_has_slash);
+    if(error != 0){
+        return error;
+    }
+
+    char* dst_trimmed_path = escape_path(dst_copy, &error, &dst_has_slash);
+    if(error != 0){
+        return error;
+    }
+
+    if (strlen(src_trimmed_path) == 1){
+        return EISDIR;
+    }
+
+    char** src_path_and_name = get_path_and_name(src_trimmed_path);
+    char* src_dir_path = src_path_and_name[0];
+    char* src_dir_name = src_path_and_name[1];
+
+    char** dst_path_and_name = get_path_and_name(dst_trimmed_path);
+    char* dst_dir_path = dst_path_and_name[0];
+    char* dst_dir_name = dst_path_and_name[1];
+
+
+    if(strlen(src_dir_name) > EXT2_NAME_LEN || strlen(dst_dir_name) > EXT2_NAME_LEN){
+        return ENAMETOOLONG;
+    }
+
+    // 2. Validate src path 
+    unsigned int inode = find_last_inode(src_dir_path, &error);
+    if(error != 0){
+        return error;
+    }
+
+    int check = check_current_inode(inode, src_dir_name);
+    if(check == 0){
+        return ENOENT;
+    }
+    if (check == 1){
+        return EISDIR;
+    }
+    else if (check == 3){
+        return ENOENT;
+    }
+    // dst path
+    unsigned int dst_inode = find_last_inode(dst_dir_path, &error);
+    if(error != 0){
+        return error;
+    }
+
+    int dst_check = check_current_inode(inode, dst_dir_name);
+    if(dst_check != 0){
+        return EEXIST;
+    }
+    
+
 
     return 0;
 }
