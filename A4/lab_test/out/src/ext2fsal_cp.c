4fd1d35df1ddbdc9731aa3ae4e005c0478c51eaa
William Gao <gaoyi.william@gmail.com>
2021-12-09 09:14:48 +0800

    trying

--- A4/lab_test/out/src/ext2fsal_cp.c
+++ A4/lab_test/out/src/ext2fsal_cp.c
@@ -47,7 +47,6 @@ int32_t ext2_fsal_cp(const char *src,
     dst_copy[strlen(dst)] = '\0';
     char* trimmed_dst = escape_path(dst_copy, &error, &has_slash);
     if(error != 0){
-        return 100;
         return error;
     }
     // processing src
@@ -88,33 +87,31 @@ int32_t ext2_fsal_cp(const char *src,
         } 
         inode = find_last_inode(dst_path, &error);
         if(error != 0){
-            return 102;
+            
             return error;
         }
         int check = check_current_inode(inode, dst_name);
 
         // not found or is a link 
         if (check == 0 || check == 3){
-            return 103;
+            
             return ENOENT;
         }
         // found a dir but doesn't have a slash
         else if (check == 1){
-            return 104;
+            
             return ENOENT;
         }
         // found a file but has a slash 
         else if (check == 2 && has_slash){
-            return 105;
             return ENOENT;
         }
     }
     
     // get ptr and size of src file
     long long size = 0;
-    char* source = get_source(src_copy, &size, &error);
+    char* source = get_source(src, &size, &error);
     if(error != 0){
-    
         return error;
     }
     
